b5a1d3c45321ffb856cc61379cd944423d207dbd
steve.borenstein@colorado.edu <steve.borenstein@colorado.edu>
2022-11-09 14:35:24 -0700

    png wind sims

--- ArduPlane/Glider/png_tests.sh
+++ ArduPlane/Glider/png_tests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-rm -rf png_logs
+rm -rf png_test
 rm -rf logs
 rm -rf kml
 mkdir kml
 
-for i in $(seq 1 1); do
+for i in $(seq 1 2); do
+    cp -rf ./missions_png_1122/wind$i.lua ./scripts/wind.lua
    (nice ./runtest_png_1122.py --mission $i --no-ui &&
-   nice mavtogpx.py ./png_logs/logs/$(date +%Y-%m-%d)/flight$i/flight.tlog &&
-   nice gpsbabel -t -w -i gpx -f ./png_logs/logs/$(date +%Y-%m-%d)/flight$i/flight.tlog.gpx  -o kml,units=m,floating=1,extrude=1 -F ./kml/flight$i.kml) &
+   nice mavtogpx.py ./png_test/logs/$(date +%Y-%m-%d)/flight$i/flight.tlog &&
+   nice gpsbabel -t -w -i gpx -f ./png_test/logs/$(date +%Y-%m-%d)/flight$i/flight.tlog.gpx  -o kml,units=m,floating=1,extrude=1 -F ./kml/flight$i.kml) &
 #   (nice ./graphs/filter.sh test_runs/mission$i.bin &&
 #    nice ./graphs/graph_logs.py --mission $i test_runs/mission$i-glide.bin &&
 #    nice ./tocsv.sh test_runs/mission$i-glide.bin) &
